489523b1f87822e1f2556e3d1670c2c0328b1794
DanielAndreasen <daniel.andreasen@astro.up.pt>
2017-12-19 14:37:34 +0000

    Add a few tests

---
+++ sap/tests/test_util.py
@@ -0,0 +1,27 @@
+import pytest
+import pandas as pd
+from utils import find_star, read_star
+
+
+def test_find_star_exists():
+    star = 'HD16008'
+    actual = find_star(star)
+    expected = 'linelist/HD16008_rv.moog'
+    assert actual == expected
+
+
+def test_find_star_not():
+    star = 'wrong'
+    with pytest.raises(IOError):
+        actual = find_star(star)
+
+
+def test_read_star():
+    fname = find_star('HD16008')
+    df = read_star(fname)
+
+    assert df.shape[0] > 100
+    assert df.shape[1] == 2
+    assert type(df) ==  pd.DataFrame
+    assert df.columns[0] == 'wavelength'
+    assert df.columns[1] == 'EW'
